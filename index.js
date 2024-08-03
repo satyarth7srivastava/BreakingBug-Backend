f066e4fd76b6a2706f60843fd3f6011b9fc17f79
satyarth7srivastava <143537599+satyarth7srivastava@users.noreply.github.com>
2024-08-03 13:01:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const Routes = require("./routes/route.js")
 const PORT = process.env.PORT || 5000
 //first comment by ss
 //second commit by ss
+//trying to sync
 dotenv.config();
 
 app.use(express.json({ limit: '10mb' }))
